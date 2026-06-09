e3d4273aaf51d11ff170c477e14352934e707b2b
CrazyYars <101417812+JagaroS@users.noreply.github.com>
2026-06-08 20:50:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot activo ✅');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor activo en puerto ${PORT}`);
+});
+
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
